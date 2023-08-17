95c6f5d5ed9e7be279d4ab468dfc4648098daa75
syam <msyamsundarsai@gmail.com>
2023-08-17 15:42:51 +0530

    adding one function into the server file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose")
 const cors = requre("cors")
-let name="sai"
+let name = "sai"
+
+const randomFunction = (type) => {
+    if (type == "true")
+    {
+        console.log('this is true condition')
+        }
+    else {
+        console.log('this is fasle condition')
+        }
+}
+
+
+randomFunction()
